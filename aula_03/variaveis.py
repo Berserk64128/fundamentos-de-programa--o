b01ac0ed5d91dffaf7ce9ba061972fa2735d4bcb
Arthur <seu_email@example.com>
2026-03-17 09:31:57 -0300

    Subindo os primeiros arquivos

---
+++ aula_03/variaveis.py
@@ -0,0 +1,13 @@
+# Variáveis e Tipos de Dados:
+# Em python, você não precisa declarar o tipo de uma variável antes de usá-la. O tipo de uma variável antes de usá-la. O tipo é inferido automaticamente pelo interpretador.
+# Em python, a variavel é declarada no padrão snake_case.
+# 1. string ->
+# 2. number - int (numero inteiro) ->
+# 3. number - float (número decimal) ->
+# 4. boolen - True ou False (Verdadeiro ou Falso) ->
+nome = "William" # string
+email = "william@gmail.com" # string
+cidade = "Rio de Janeiro" #string
+cpf = 14865797898 # number-int
+salario = 14000.65 # number-float
+casado = False # boolean 
---
+++ aula_04/operadores.py
@@ -0,0 +1,34 @@
+# 1. Operadores Matemáticos
+#potencia = **
+#porcentagem = %
+#shift + alt + setinha = duplica
+#ctrl + D = mudar textos iguas
+
+# soma
+numero_um = 20
+numero_dois = 15
+
+soma = numero_um + numero_dois
+print(f"A soma entre {numero_um} + {numero_dois} = {soma}")
+
+#subtração
+numero_tres =  49
+numero_quatro = 29
+
+soma2 = numero_tres - numero_quatro
+print("a resposta:", soma2)
+
+#multiplicação
+numero_cinco = 47
+numero_seis = 10
+
+soma3 = numero_cinco * numero_seis
+print(soma3)
+
+#fração
+numero_sete = 24
+numero_oito = 2
+
+soma4 = numero_sete / numero_oito
+print(soma4)
+
