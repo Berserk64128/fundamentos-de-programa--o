d5aea52a7d76d584e293e5477c62087ba8b760cb
Arthur Seabra <art64sm@gmail.com>
2026-06-02 09:52:39 -0300

    Subindo 02/06

--- aula_17/estrutura_def_ret.py
+++ aula_17/estrutura_def_ret.py
@@ -1,21 +1,21 @@
 # Função que exibe os sabores, tamanhos e valores das pizzas
-def exibir_cardapio():
-    print("=== CARDÁPIO PIZZARIA DO CÓDIGO ===")
-    print("Margherita - P: R$25 | M: R$35 | G: R$45")
-    print("Calabresa - P: R$28 | M: R$38 | G: R$48")
-    print("Frango - P: R$30 | M: R$40 | G: R$50")
+# def exibir_cardapio():
+#     print("=== CARDÁPIO PIZZARIA DO CÓDIGO ===")
+#     print("Margherita - P: R$25 | M: R$35 | G: R$45")
+#     print("Calabresa - P: R$28 | M: R$38 | G: R$48")
+#     print("Frango - P: R$30 | M: R$40 | G: R$50")
 
 # exibir_cardapio()
 
 # Função para aplicar desconto, onde o preço e o percentual de desconto será passado no momento da invocação da função
-valor_sem_desc = 40
+# valor_sem_desc = 40
 
-def aplicar_desconto(preco, percentual):
-    # preco * (1 - percentual /100)
-    return preco * percentual
+# def aplicar_desconto(preco, percentual):
+#     # preco * (1 - percentual /100)
+#     return preco * percentual
 
-preco_final = valor_sem_desc - aplicar_desconto(valor_sem_desc, 0.10)
-print(f"Preço com desconto: R$ {preco_final:.2f}")
+# preco_final = valor_sem_desc - aplicar_desconto(valor_sem_desc, 0.10)
+# print(f"Preço com desconto: R$ {preco_final:.2f}")
 
 # Declarar função que receberá por padrão que a borda não é recheada. Além disso, irá receber também o sabor e tamanho da pizza com  padrão "M".
 def fazer_pedido(sabor, tamanho="M", borda_recheada=False):
